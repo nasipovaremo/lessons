4f49e352c0f38090793d2b3f2264b9b78f6190be
nasipovaremo <nasipovaown@gmail.com>
2023-05-15 18:17:50 +0600

    initial

---
+++ Lesson1.py/ifelse.py
@@ -0,0 +1,95 @@
+# Задачки
+# 1---------------------------------
+# a = 8
+# b = 2
+# if a > b:
+#     print("a >")
+# elif b > a:
+#     print("b>")
+# else:
+#     print("они равны")
+
+
+# 2---------------------------------
+# num = int(input("Введите число: "))
+# if num < 0:
+#     print(num*(-1))
+# else:
+#     print(num)
+
+# 3----------------------------------
+# num1 = int(input("ВВедите первое число:"))
+# num2 = int(input("Введите второе число:"))
+# if num1 == num2:
+#     print("No")
+# else:
+#     print('Yes')
+
+# 4--------------------------------
+# num = int(input("Какое-то число:"))
+# if num > 100 or num <-100:
+#     print("-")
+# else:
+#     print("+")
+
+# 5-----------------------------
+# month = int(input("Введи номер месяца:"))
+# if month == 12 or month == 1 or month == 2:
+#     print("winter")
+# elif month == 3 or month == 4 or month == 5:
+#    print("spring")
+# elif month == 6 or month == 7 or month == 8:
+#     print("summer")
+# elif month == 9 or month == 10 or month == 11:
+#     print('autumn')
+# else:
+#     print("Ты что! нужно ввести число!!!")
+
+# 6------------------------------
+# num1=int(input("Введите число:"))
+# num2=int(input("Введите число:"))
+# num3 = int(input("Введите число:"))
+# if num1 > 10 and num2 > 10 and num3 > 10:
+#     print("Yes")
+# else: 
+#     print("No")
+
+# 7--------------------------
+# a = -1
+# b = -5
+# c = 3
+# count = 0 
+# if a > 0:
+#     count += 1
+# if b > 0:
+#     count += 1
+# if c > 0:
+#     count += 1
+# print(count)
+
+# 8-------------------------------
+# mounth = int(input("Введите количествто месяцев: "))
+# years = int(input("Введите количествто годов: "))
+# days = (mounth*29) + (years*365)
+# print(days)
+
+# Деление---------------------------
+# first_num  = int(input("Введите число: "))
+# if first_num % 3 == 0 and 5 == 0:
+#     print("Ого,делится!")
+# elif first_num % 3 == 0:
+#     print("Ара-ара")
+# elif first_num % 5== 0:
+#     print("Error")
+
+# # Военком
+# age =45
+# if age <=16:
+#     print("Еще рано...")
+# elif age >=18 and age <=40:
+#     print("Идем служить!")
+# elif age >=40:
+#     print('Уже не надо')    
+# else:
+#     print("Error")
+
---
+++ Lesson2.py/Spiski i metody spiskov.py
@@ -0,0 +1,89 @@
+# list = []
+# a = []  
+# b = [34,45] 
+# a.append(True)
+# a.append(23)
+# a.append(b)
+# print(a[1])
+
+# a = [45]
+# b = [78]
+# list.append(True)
+# list.append(b)
+# print(b[1])
+
+
+# def simple_multiplication(number):
+#     if number % 2 == 0:
+#         return number * 8
+#     elif number % 2 != 0:
+#         return number * 9
+
+# print(simple_multiplication(4))
+
+# def double_char(s):
+#     out = ''
+#     for i in s:
+#            out = out+i*2
+#     return out 
+
+# print(double_char('hello'))
+
+# def make_upper_case(s):
+#     s = s.upper()
+#     return s
+
+
+# def check(seq, elem):
+    # if elem in seq:
+        # return True
+    # else:
+        # return False
+    
+
+    # def map(a)/:
+    #  retirn 
+
+# def count_sheepa(n):
+#     for i in range(n);
+#         print(f'{i}sheep...' , end= "")
+
+
+# name = input()
+# for s in iter(input, '0'):
+#       print(f'{name} № {s[-1]}')
+
+
+
+
+# n=int(input())
+
+# for i in range(n,0,-1):
+#   print(i,end=' ')
+
+# list('spisok
+
+# name = input("What is your name?")
+# print(Hi, name)
+
+# a = 1; b = 2; print(a,b)
+ 
+
+# print ("Здравствуйте, укажите ваше имя.")
+# s = input ("Имя: ", )
+# if s == 'TVS' : print ("Приветствую, (тут типо ваше имя).") ("Авторизация")
+# else: print ("Такого пользователя нет")
+# print ("Введите пароль.")
+# s = input ("Пароль:")
+# if s == '1234': print ("Добро пожаловать!")
+# else: print ("Неправильный пароль.")
+
+# contant_name = {
+#     'asel': 996778459156,
+#     'fatima': 99854821144,
+#     "scorpio": 2012,
+# }
+# print(contant_name['scorpio'])
+
+
+import array
